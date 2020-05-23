cb2e57e80fb4d374534124ce1e12ffff6d538e47
Puneet Pugalia <puneet.pugalia@gmail.com>
2020-05-23 12:23:09 +0530

    A couple of buttons which can open popups

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,8 +9,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var alertIsVisible:Bool = false;
+    @State var whosThereisVisible:Bool = false;
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            VStack {
+                
+                Text("Welcome to my first app!")
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.green)
+                
+                Button(action: {
+                    print("Button Pressed")
+                    self.alertIsVisible = true;
+                }) {
+                    Text("Hit me!")
+                }
+                .alert(isPresented: $alertIsVisible) { () ->
+                    Alert in
+                    return Alert(
+                        title: Text("Hello There"),
+                        message: Text("This is my first popup."),
+                        dismissButton: .default(Text("Awesome!"))
+                    )
+                }
+                
+                Button(action: {
+                    self.whosThereisVisible = true;
+                }) {
+                    Text("Knock Knock")
+                }
+                .alert(isPresented: $whosThereisVisible) { () ->
+                    Alert in
+                    return Alert(title: Text("Who's there?"), message: Text("A knock knock joke!"), dismissButton: .default(Text("Nice joke!")))
+                }
+            }
+        }
     }
 }
 
