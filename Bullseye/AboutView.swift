ec6e9d75162aa905bc10c798c073076714b063b6
Puneet Pugalia <puneet.pugalia@gmail.com>
2020-05-24 08:35:09 +0530

    Styling the about view

--- Bullseye/AboutView.swift
+++ Bullseye/AboutView.swift
@@ -9,14 +9,42 @@
 import SwiftUI
 
 struct AboutView: View {
-    var body: some View {
-        VStack {
-            Text("🎯 Bullseye 🎯")
-            Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.")
-            Text("Your goal is to place the slider as close as possible to the target value. The closer you are the more points you score.")
-            Text("Enjoy!")
+    
+    let beige = Color(red: 255.0/255.0, green: 214.0/255.0, blue: 179.0/255.0)
+    
+    struct headingStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(Color.black)
+                .font(Font.custom("Arial Rounded MT Bold", size: 30))
+                .padding(.top, 60)
+                .padding(.bottom, 60)
         }
-        .navigationBarTitle("About Bullseye")
+    }
+    
+    struct bodyStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(Color.black)
+                .font(Font.custom("Arial Rounded MT Bold", size: 16))
+                .padding(.leading, 60)
+                .padding(.trailing, 60)
+                .padding(.bottom, 20)
+        }
+    }
+    
+    var body: some View {
+        Group {
+            VStack {
+                Text("🎯 Bullseye 🎯").modifier(headingStyle())
+                Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.").modifier(bodyStyle())
+                Text("Your goal is to place the slider as close as possible to the target value. The closer you are the more points you score.").modifier(bodyStyle())
+                Text("Enjoy!").modifier(bodyStyle())
+            }
+            .background(beige)
+            .navigationBarTitle("About Bullseye")
+        }
+        .background(Image("Background"))
     }
 }
 
